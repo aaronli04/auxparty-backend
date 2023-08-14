65d651b6e77be3a67ae07c9fbc97461b6020974f
Aaron <aaronli5260@gmail.com>
2023-08-14 18:23:53 -0500

    create room adjustment

--- src/shared/rooms/createRoom.ts
+++ src/shared/rooms/createRoom.ts
@@ -1,5 +1,6 @@
 import supabase from "../../supabase/client";
 import { Room, StringKeyMap } from "../../types";
+import { getRoomByName } from "./getRoomByName";
 
 export async function createRoom(payload: StringKeyMap): Promise<Room | null> {
     const { auxpartyId, roomName, roomPassword } = payload
@@ -11,10 +12,16 @@ export async function createRoom(payload: StringKeyMap): Promise<Room | null> {
         .from('rooms')
         .select()
         .eq('auxpartyId', auxpartyId)
-        .limit(1);
+        .limit(1)
 
     if (fetchError || existingRoom.length > 0) {
-        return null;
+        return null
+    } 
+
+    // Check if room with name already exists
+    const roomSameName = await getRoomByName(roomName)
+    if (roomSameName) {
+        return null
     }
 
     // Create room
@@ -24,5 +31,5 @@ export async function createRoom(payload: StringKeyMap): Promise<Room | null> {
         .select()
 
     if (error || data.length === 0) { return null }
-    return data[0];
+    return data[0]
 }
